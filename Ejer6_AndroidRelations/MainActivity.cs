db553a9a9efa1e40dd41aed8110e96f08e852a57
Natalia <Natalia@NATALIA-HP>
2020-01-09 16:56:07 +0100

    ejer 1 del ejercicio 6

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -6,13 +6,15 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace Ejer6_AndroidRelations
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        private Button MuestraDialog;
+        private EditText edittext;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,7 +25,20 @@ namespace Ejer6_AndroidRelations
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+            MostrarEditText();
+
+            edittext.KeyPress += (object sender, View.KeyEventArgs e) =>
+              {
+                  e.Handled = false;
+                  if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                  {
+                      Toast.MakeText(this, edittext.Text, ToastLength.Short).Show();
+                      e.Handled = true;
+                  }
+              };
         }
+
+       
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
@@ -48,6 +63,22 @@ namespace Ejer6_AndroidRelations
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
-	}
+        private void MuestraDialogo(object sender,EventArgs args)
+        {
+
+            string texto = edittext.Text;
+            AlertDialog.Builder VentanaAlerta = new AlertDialog.Builder(this);
+            VentanaAlerta.SetMessage(texto);
+            VentanaAlerta.SetTitle(texto);
+            VentanaAlerta.Show();
+        }
+        private void MostrarEditText()
+        {
+            edittext = FindViewById<EditText>(Resource.Id.textoPrincipal);
+            MuestraDialog = FindViewById<Button>(Resource.Id.boton);
+
+            MuestraDialog.Click += MuestraDialogo;
+        }
+    }
 }
 
