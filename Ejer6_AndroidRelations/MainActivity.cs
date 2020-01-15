a52986f10480833ee3774ae65fa3bf65d6f80e7d
Natalia <Natalia@NATALIA-HP>
2020-01-15 15:50:57 +0100

    Cambio Vuelta a atras

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -6,28 +6,77 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace Ejer6_AndroidRelations
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        private T FindViewById<T>(int fab)
+        private Button MuestraDialog;
+        private EditText edittext;
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            throw new NotImplementedException();
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+            MostrarEditText();
+
+            edittext.KeyPress += (object sender, View.KeyEventArgs e) =>
+              {
+                  e.Handled = false;
+                  if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                  {
+                      Toast.MakeText(this, edittext.Text, ToastLength.Short).Show();
+                      e.Handled = true;
+                  }
+              };
         }
 
-        private void SetContentView(int activity_main)
+        public override bool OnCreateOptionsMenu(IMenu menu)
         {
-            throw new NotImplementedException();
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
         }
 
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
+            View view = (View)sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
-	}
+        private void MuestraDialogo(object sender, EventArgs args)
+        {
+
+            string texto = edittext.Text;
+            AlertDialog.Builder VentanaAlerta = new AlertDialog.Builder(this);
+            VentanaAlerta.SetMessage(texto);
+            VentanaAlerta.SetTitle(texto);
+            VentanaAlerta.Show();
+        }
+        private void MostrarEditText()
+        {
+            edittext = FindViewById<EditText>(Resource.Id.textoPrincipal);
+            MuestraDialog = FindViewById<Button>(Resource.Id.bottom);
+
+            MuestraDialog.Click += MuestraDialogo;
+        }
+    }
 }
 
