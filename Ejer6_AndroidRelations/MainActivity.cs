9e9e962db7360765197745149233ad3bed4be92b
Natalia <Natalia@NATALIA-HP>
2020-01-15 17:41:29 +0100

    Ejercicio2-3-4 completados

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -13,8 +13,18 @@ namespace Ejer6_AndroidRelations
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        private Button MuestraDialog;
-        private EditText edittext;
+        public EditText textoEditado;
+        private Button boton1;
+        private Button boton2;
+        private Button boton3;
+        private Button boton4;
+        private Button boton5;
+        private Button boton6;
+        private Button boton7;
+        private Button boton8;
+        private Button boton9;
+        public Button botonConfirmacion;
+    
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,59 +34,51 @@ namespace Ejer6_AndroidRelations
             SetSupportActionBar(toolbar);
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-            MostrarEditText();
-
-            edittext.KeyPress += (object sender, View.KeyEventArgs e) =>
-              {
-                  e.Handled = false;
-                  if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
-                  {
-                      Toast.MakeText(this, edittext.Text, ToastLength.Short).Show();
-                      e.Handled = true;
-                  }
-              };
+                
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        private void AgregarFuncionalidad()
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
+            boton1 = FindViewById<Button>(Resource.Id.botonuno);
+            boton2 = FindViewById<Button>(Resource.Id.botondos);
+            boton3 = FindViewById<Button>(Resource.Id.botontres);
+            boton4 = FindViewById<Button>(Resource.Id.botoncuatro);
+            boton5 = FindViewById<Button>(Resource.Id.botoncinco);
+            boton6 = FindViewById<Button>(Resource.Id.botonseis);
+            boton7 = FindViewById<Button>(Resource.Id.botonsiete);
+            boton8 = FindViewById<Button>(Resource.Id.botonocho);
+            boton9 = FindViewById<Button>(Resource.Id.botonnueve);
+            botonConfirmacion = FindViewById<Button>(Resource.Id.botonConfirmacion);
+
+            boton1.Click += colocarTexto;
+            boton2.Click += colocarTexto;
+            boton3.Click += colocarTexto;
+            boton4.Click += colocarTexto;
+            boton5.Click += colocarTexto;
+            boton6.Click += colocarTexto;
+            boton7.Click += colocarTexto;
+            boton8.Click += colocarTexto;
+            boton9.Click += colocarTexto;
+            botonConfirmacion.Click += confirmacion;
+        }
+        private void colocarTexto(object sender,EventArgs e)
         {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            Button botonElegido = (Button)sender;
+            textoEditado.Text = textoEditado.Text += botonElegido.Text;
+        }
+       private void confirmacion(object sender,EventArgs e)
+        {
+            if (textoEditado.Text.Equals("1234"))
             {
-                return true;
+                SetContentView(Resource.Layout.activity_main);
+                
             }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-        private void MuestraDialogo(object sender, EventArgs args)
-        {
-
-            string texto = edittext.Text;
-            AlertDialog.Builder VentanaAlerta = new AlertDialog.Builder(this);
-            VentanaAlerta.SetMessage(texto);
-            VentanaAlerta.SetTitle(texto);
-            VentanaAlerta.Show();
-        }
-        private void MostrarEditText()
-        {
-            edittext = FindViewById<EditText>(Resource.Id.textoPrincipal);
-            MuestraDialog = FindViewById<Button>(Resource.Id.bottom);
-
-            MuestraDialog.Click += MuestraDialogo;
-        }
+            else
+            {
+                textoEditado.SetTextColor(Android.Graphics.Color.Red);
+            }
+        }    
     }
 }
 
